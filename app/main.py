918a906780e8f5321ae3b32fd2a4c225d303f184
Ronnie Luyo <ronnie.luyo25@gmail.com>
2026-03-15 11:16:06 -0500

    corregir CORS para Vercel

--- app/main.py
+++ app/main.py
@@ -10,7 +10,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://ft-tutores-react.vercel.app",  # ← tu URL de Vercel
+        "https://portal-tutor-friendteacher.vercel.app",
+        "https://portal-tutor-friendteacher-la8d1rzky-alexander-sks-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
